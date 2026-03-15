50ec883136baa8da9d94b78b884ebfd8ada7b406
Bashir Swed <bashirswed3@gmail.com>
2026-03-16 02:17:22 +0300

    ..

--- src/SlugGenerator/SlugGenerator/SlugGenerator.cs
+++ src/SlugGenerator/SlugGenerator/SlugGenerator.cs
@@ -6,7 +6,38 @@ namespace SlugGenerator
     {
         public static string GenerateSlug(string input)
         {
-            throw new NotImplementedException();
+            string slug = string.Empty;
+            try
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsLetter(input[i]))
+                    {
+                        slug += char.ToLower(input[i]);
+                    }
+                    else if((i==0 || i==input.Length-1)&&!char.IsLetter(input[i]))
+                    {
+                        continue;
+
+                    }
+                    else if (!char.IsLetter(input[i]) && !char.IsLetter(input[i - 1]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        slug += '-';
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+
+            }
+            return new string(slug);
+            
         }
     }
 }
