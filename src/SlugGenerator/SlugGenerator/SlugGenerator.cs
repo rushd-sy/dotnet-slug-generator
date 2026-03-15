360dc8b7d1c032c046f55d5764d658843addc05d
Bashir Swed <bashirswed3@gmail.com>
2026-03-15 18:41:17 +0300

    refactor : remove unused using and throw NotImplementedException

--- src/SlugGenerator/SlugGenerator/SlugGenerator.cs
+++ src/SlugGenerator/SlugGenerator/SlugGenerator.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SlugGenerator
 {
-    public class SlugHelper
+    public class SlugGenerator
     {
         public static string GenerateSlug(string input)
         {
