b6ec9dcbf7304513c8a0fe3e493ebdacc6e778aa
Bashir Swed <bashirswed3@gmail.com>
2026-05-01 04:50:34 +0300

    .

--- src/SlugGenerator/SlugGenerator/SlugGenerator.cs
+++ src/SlugGenerator/SlugGenerator/SlugGenerator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using System.Linq;
+﻿using System.Text.RegularExpressions;
 
 namespace SlugGenerator
 {
@@ -13,9 +11,7 @@ namespace SlugGenerator
                 return string.Empty;
             }
 
-            string slug = input;
-
-            slug = Regex.Replace(Regex.Replace(input.ToLower().Trim().Replace(" ",slugBy.ToString()).Replace("_",slugBy.ToString())
+            string slug = Regex.Replace(Regex.Replace(input.ToLower().Trim().Replace(" ",slugBy.ToString()).Replace("_",slugBy.ToString())
                 , @"[^a-z0-9" + Regex.Escape(slugBy.ToString()) + "]", "")
                 , $@"{Regex.Escape(slugBy.ToString())}+", $"{slugBy}").Trim(slugBy);
 
---
+++ tests/TestSlug/TestSlug/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SlugGenerator;
+
+namespace SlugGenerator {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string test = Console.ReadLine();
+           Console.WriteLine( test.GenerateSlug('-'));
+
+        }
+    }
+}
